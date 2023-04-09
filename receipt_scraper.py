f3b998ba157018d7ff6d2211e9945cb8a14acf74
Alexander Feng <afen8893@uni.sydney.edu.au>
2023-04-09 15:26:41 +1000

    able to enter email in log in

--- receipt_scraper.py
+++ receipt_scraper.py
@@ -1,6 +1,25 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import login_details
+import undetected_chromedriver as webdriver
+import time
 
-service = Service('C:\\Users\\Alex Feng\\chromedriver_win32\\chromedriver.exe')
-driver = webdriver.Chrome(service=service)
-driver.get('https://www.google.com/')
+driver = webdriver.Chrome()
+
+url = 'https://accounts.woolworthsrewards.com.au/er-login/validate-user?referrer=REWARDS_CP&state=state123'
+driver.get(url)
+driver.implicitly_wait(10) # gives an implicit wait for 20 seconds
+# html_source = driver.page_source
+# soup = BeautifulSoup(html_source, 'html.parser')
+# pretty_html = soup.prettify()
+# print(pretty_html)
+
+
+driver.find_element(By.XPATH, '//*[@id="emailCardNumber"]').send_keys(login_details.email)
+driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-user-email-card-number/form/div['
+                              '3]/button[1]').click()
+
+
+time.sleep(20)  # add a 5-second delay
+driver.quit()
