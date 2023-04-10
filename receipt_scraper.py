aca3b0d1bd61005a6264607a1318761f7b8125c9
Alexander Feng <afen8893@uni.sydney.edu.au>
2023-04-10 18:38:16 +1000

    woolies is too good with their bot detection. send code to email screen is not able to be accessed by the scraper so work around, input the otp manually and proceed to perform the scraping from there. gmail_get_recent_email is no longer useful but still there in case I want to explore in the future.

--- receipt_scraper.py
+++ receipt_scraper.py
@@ -1,25 +1,48 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import login_details
 import undetected_chromedriver as webdriver
 import time
+from gmail_get_recent_email import main
 
 driver = webdriver.Chrome()
 
-url = 'https://accounts.woolworthsrewards.com.au/er-login/validate-user?referrer=REWARDS_CP&state=state123'
+url = 'https://www.woolworthsrewards.com.au/#login'
 driver.get(url)
-driver.implicitly_wait(10) # gives an implicit wait for 20 seconds
-# html_source = driver.page_source
-# soup = BeautifulSoup(html_source, 'html.parser')
-# pretty_html = soup.prettify()
-# print(pretty_html)
 
+# Switch to the iframe (assuming the iframe has a name or ID attribute)
+iframe = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//*[@id="WXLoginIFrameObject"]')))
+driver.switch_to.frame(iframe)
 
+# input login email
 driver.find_element(By.XPATH, '//*[@id="emailCardNumber"]').send_keys(login_details.email)
 driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-user-email-card-number/form/div['
                               '3]/button[1]').click()
 
+# find enter password and click
+# password_check_box = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//*[@id="mat-radio-2"]')))
+# password_check_box.click()
+
+password_check_confirm = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-choose-login/form/div[3]/button[1]')))
+password_check_confirm.click()
+
+# input password
+# password_input = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//*[@id="password"]')))
+# password_input.send_keys(login_details.password)
+# driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-password/form/div[4]/button[1]').click()
+
+# time.sleep(5)
+# driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-password-secure-verification/form/div[3]/form/button').click()
+# time.sleep(15)
+otp = input('Enter SMS OTP:')
+# email_verification_otp = main()
+driver.find_element(By.XPATH, '//*[@id="otp"]').send_keys(otp)
+driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-one-time-pass/form/div[5]/button[1]').click()
 
 time.sleep(20)  # add a 5-second delay
 driver.quit()
+
+
