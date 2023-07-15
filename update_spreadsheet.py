f9f08a0dd288933bf9312877c7642b1e24bd138a
Alex <alexander.feng2@gmail.com>
2023-07-15 17:00:30 +1000

    restructured files

---
+++ update_spreadsheet.py
@@ -0,0 +1,19 @@
+# from receipts.receipt_scraper import something
+
+if __name__ == "__main__":
+
+    # Log in
+
+    # Scrape receipts
+
+    # Move receipts
+
+    # Read receipts into table from drive
+
+    # Add weightings from existing weighting file
+
+    # Prompt user to fill out weighting for new items
+
+    # update weighting file with new weighting
+
+    pass
