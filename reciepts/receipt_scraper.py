fdbc163ae5739cf92d869d9ea7e8ca585908d64b
Alex <alexander.feng2@gmail.com>
2023-07-15 22:04:46 +1000

    removed manual datetime and added date to read receipts up to

--- reciepts/receipt_scraper.py
+++ reciepts/receipt_scraper.py
@@ -8,81 +8,85 @@ from datetime import datetime
 
 import login_details
 
-month_dict = dict([(1, 'Jan'), (2, 'Feb'), (3, 'Mar'), (4, 'Apr'), (5, 'May'), (6, 'Jun'),
-                   (7, 'Jul'), (8, 'Aug'), (9, 'Sep'), (10, 'Oct'), (11, 'Nov'), (0, 'Dec')])
 
-driver = webdriver.Chrome()
+def receipt_scraper(date_up_to: datetime):
+    if not isinstance(date_up_to, datetime):
+        raise TypeError("Expected argument 'date_up_to' to be a datetime object.")
 
-url = 'https://www.woolworthsrewards.com.au/#login'
-driver.get(url)
+    driver = webdriver.Chrome()
 
+    url = 'https://www.woolworthsrewards.com.au/#login'
+    driver.get(url)
 
+    # Switch to the iframe (assuming the iframe has a name or ID attribute)
+    iframe = WebDriverWait(driver, 10).until(
+        ec.presence_of_element_located((By.XPATH, '//*[@id="WXLoginIFrameObject"]')))
+    driver.switch_to.frame(iframe)
 
-# Switch to the iframe (assuming the iframe has a name or ID attribute)
-iframe = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//*[@id="WXLoginIFrameObject"]')))
-driver.switch_to.frame(iframe)
+    # input login email
+    email_input = WebDriverWait(driver, 31).until(
+        ec.presence_of_element_located((By.XPATH, '//*[@id="emailCardNumber"]')))
+    email_input.send_keys(login_details.email)
+    driver.find_element(By.XPATH,
+                        '/html/body/erl-root/div/erl-validate-user/div/div/erl-user-email-card-number/form/div['
+                        '3]/button[1]').click()
 
-# input login email
-email_input = WebDriverWait(driver, 30).until(ec.presence_of_element_located((By.XPATH, '//*[@id="emailCardNumber"]')))
-email_input.send_keys(login_details.email)
-driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-user-email-card-number/form/div['
-                              '3]/button[1]').click()
+    # click sms verification button
+    verify_check_confirm = WebDriverWait(driver, 10).until(ec.presence_of_element_located(
+        (By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-choose-login/form/div[3]/button[1]')))
+    verify_check_confirm.click()
 
-# click sms verification button
-verify_check_confirm = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-choose-login/form/div[3]/button[1]')))
-verify_check_confirm.click()
+    # input otp
+    otp = input('Enter SMS OTP:')
+    driver.find_element(By.XPATH, '//*[@id="otp"]').send_keys(otp)
+    driver.find_element(By.XPATH,
+                        '/html/body/erl-root/div/erl-validate-user/div/div/erl-one-time-pass/form/div[5]/button[1]').click()
 
-# input otp
-otp = input('Enter SMS OTP:')
-driver.find_element(By.XPATH, '//*[@id="otp"]').send_keys(otp)
-driver.find_element(By.XPATH, '/html/body/erl-root/div/erl-validate-user/div/div/erl-one-time-pass/form/div[5]/button[1]').click()
+    # get to receipt page
+    my_account = WebDriverWait(driver, 20).until(ec.presence_of_element_located(
+        (By.XPATH, '/html/body/div[4]/div[1]/header[1]/nav[1]/div/div/div[1]/ul/li[5]/a')))
+    my_account.click()
+    my_activity = WebDriverWait(driver, 20).until(
+        ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[1]/header[1]/nav[2]/div/div/div/ul/li[1]/a')))
+    my_activity.click()
 
-# get to receipt page
-my_account = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[1]/header[1]/nav[1]/div/div/div[1]/ul/li[5]/a')))
-my_account.click()
-my_activity = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[1]/header[1]/nav[2]/div/div/div/ul/li[1]/a')))
-my_activity.click()
+    # Downloading receipts
 
-# Downloading receipts
-max_counter = 10
-current_month = datetime.now().month
-previous_month_dict_key = current_month-1
-current_month_str = month_dict[current_month]
-previous_month_str = month_dict[previous_month_dict_key]
+    max_counter = 10
 
-for receipt_num in range(2, max_counter):
-    receipt_xpath = '//*[@id="angular-view-div"]/div/div[5]/wr-my-activity-new-element/div/div/div[3]/div[' + str(receipt_num) + ']'
-    receipt_date_xpath = receipt_xpath + '/div[1]/div/div/div[1]'
-    receipt_date = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, receipt_date_xpath))).text
-    receipt_date_month = receipt_date[-3:]
+    for receipt_num in range(2, max_counter):
+        receipt_xpath = '//*[@id="angular-view-div"]/div/div[5]/wr-my-activity-new-element/div/div/div[3]/div[' + str(
+            receipt_num) + ']'
+        receipt_date_xpath = receipt_xpath + '/div[1]/div/div/div[1]'
 
-    if receipt_date_month == current_month_str:
-        receipt_banner = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, receipt_xpath)))
-        receipt_banner.click()
-        receipt_download = WebDriverWait(driver, 20)\
-            .until(ec.presence_of_element_located((By.XPATH, '//*[@id="ereceiptSidesheet"]/div/div/div[1]/a/img')))
-        receipt_download.click()
-        x_click_out = WebDriverWait(driver, 20)\
-            .until(ec.presence_of_element_located((By.XPATH, '//*[@id="ereceiptSidesheet"]/div/a/img')))
-        x_click_out.click()
+        # need a try except here
+        receipt_date_string = WebDriverWait(driver, 5).until(
+            ec.presence_of_element_located((By.XPATH, receipt_date_xpath))).text
+        receipt_date_month = receipt_date_string[-3:]
+        receipt_date_day = receipt_date_string[4:6]
+        if receipt_date_month == "Dec":
+            if datetime.now().year != date_up_to.year:
+                receipt_date_year = date_up_to.year
+            else:
+                receipt_date_year = datetime.now().year
+        else:
+            receipt_date_year = datetime.now().year
 
-    elif receipt_date_month == previous_month_str:
-        break
+        date_format = "%d%b%Y"
+        date_string = receipt_date_day+receipt_date_month+str(receipt_date_year)
+        receipt_date = datetime.strptime(date_string, date_format)
+        # if the current year is not the same as the inputted year and something about january
+        if receipt_date > date_up_to:
+            print(receipt_xpath)
+            receipt_banner = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, receipt_xpath)))
+            receipt_banner.click()
+            receipt_download = WebDriverWait(driver, 30).until(ec.presence_of_element_located((By.XPATH, '//*[@id="ereceiptSidesheet"]/div/div/div[1]/a/img')))
+            receipt_download.click()
+            x_click_out = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, '//*[@id="ereceiptSidesheet"]/div/a/img')))
+            x_click_out.click()
 
+        else:
+            break
 
-
-# //*[@id="angular-view-div"]/div/div[6]/wr-my-activity-new-element/div/div/div[3]/div[2]
-# //*[@id="angular-view-div"]/div/div[6]/wr-my-activity-new-element/div/div/div[3]/div[3]
-# //*[@id="angular-view-div"]/div/div[6]/wr-my-activity-new-element/div/div/div[3]/div[6]
-
-# //*[@id="angular-view-div"]/div/div[6]/wr-my-activity-new-element/div/div/div[3]/div[2]/div[1]/div/div/div[1]
-
-# //*[@id="angular-view-div"]/div/div[6]/wr-my-activity-new-element/div/div/div[3]/div[3]/div[1]/div/div/div[1]
-"""
-The way I see this happening is a conditional statement based on the html that check that the month in the html is
-the current month. if it is it'll go through the selenium motions to download and collect the pdfs
-"""
-time.sleep(20)  # add a 5-second delay
-driver.quit()
-
-
+    time.sleep(20)  # add a 5-second delay
+    driver.quit()
--- update_spreadsheet.py
+++ update_spreadsheet.py
@@ -1,7 +1,12 @@
-# from receipts.receipt_scraper import something
+from reciepts.receipt_scraper import receipt_scraper
+from datetime import datetime
 
 if __name__ == "__main__":
 
+    date = "09Jun2023"
+    date_format = "%d%b%Y"
+    date_datetime = datetime.strptime(date, date_format)
+    receipt_scraper(date_datetime)
     # Log in
 
     # Scrape receipts
