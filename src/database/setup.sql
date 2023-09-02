19d7e289c1481028c69077cb6c32214ea232b484
Alex <alexander.feng2@gmail.com>
2023-09-02 22:43:52 +1000

    setting up db container

---
+++ src/database/setup.sql
@@ -0,0 +1,17 @@
+use finance_db;
+
+CREATE SCHEMA household
+
+CREATE TABLE household.weightings(
+    item VARCHAR(50) PRIMARY KEY,
+    adam FLOAT,
+    alex FLOAT,
+    tyler FLOAT,
+    persist BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE household.items_bought(
+    item VARCHAR(50),
+    price NUMERIC(7,2),
+    payer VARCHAR(10)
+)
