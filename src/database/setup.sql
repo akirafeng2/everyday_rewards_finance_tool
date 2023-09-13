61b23eba7d9c06ba507b5172c63a93bc0cb8ce53
Alex <alexander.feng2@gmail.com>
2023-09-13 22:44:58 +1000

    Network changes

--- src/database/setup.sql
+++ src/database/setup.sql
@@ -5,10 +5,11 @@ CREATE TABLE household.weightings(
     adam FLOAT,
     alex FLOAT,
     tyler FLOAT,
-    persist BOOLEAN DEFAULT TRUE
+    persist BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE household.items_bought(
+    id SERIAL PRIMARY KEY,
     item VARCHAR(50),
     price NUMERIC(7,2),
     payer VARCHAR(10)
