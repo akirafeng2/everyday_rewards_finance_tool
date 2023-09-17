8f94b61672c651f1b9881e12bac32c98fd910f9b
Alex <alexander.feng2@gmail.com>
2023-09-18 00:02:37 +1000

    added items_and_weightings view

--- src/database/setup.sql
+++ src/database/setup.sql
@@ -9,7 +9,13 @@ CREATE TABLE household.weightings(
 );
 
 CREATE TABLE household.items_bought(
+    id SERIAL PRIMARY KEY,
     item VARCHAR(50),
     price NUMERIC(7,2),
     payer VARCHAR(10)
 );
+
+CREATE VIEW household.items_and_weightings AS
+SELECT a.id as id, a.item as item, a.price as price, a.payer as payer, b.adam as adam, b.alex as alex, b.tyler as tyler, b.persist as persist
+FROM household.items_bought as a
+LEFT JOIN household.weightings as b on a.item = b.item;
