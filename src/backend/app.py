1e4777438a5b3781469cf38a44c85850ffa321cd
Alex <alexander.feng2@gmail.com>
2023-10-22 17:10:20 +1100

    refactoring app for blueprints

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, render_template, abort, session, redirect, url_for
+from . import SETTINGS, user, receipt
 
-from backend import FileSystem, DatabaseConnection2, Calculations
-from routes.register_routes import api
-import SETTINGS
 
-env = SETTINGS.ENV
-DB_CONN = DatabaseConnection2(SETTINGS.CONNECTION_DETAILS, env, "1") # get rid or profile_id here. save it in session within views and pass it through methods of DB_CONN within views
-FS = FileSystem(SETTINGS.FINANCE_FILE_PATH / env)
+# env = SETTINGS.ENV
+# DB_CONN = DatabaseConnection2(SETTINGS.CONNECTION_DETAILS, env, "1") # get rid or profile_id here. save it in session within views and pass it through methods of DB_CONN within views
+# FS = FileSystem(SETTINGS.FINANCE_FILE_PATH / env)
 
 app = Flask(__name__)
+
 app.secret_key = SETTINGS.SECRET_KEY
-app.DB_CONN = DB_CONN
-app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(user.views.blueprint, url_prefix="/api/user")
+app.register_blueprint(receipt.views.blueprint, url_prefix="/api/receipt")
