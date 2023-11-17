02b6396a72d6d7cf554af923790e93c7b11d7479
akirafeng2 <alexander.feng2@gmail.com>
2023-11-17 19:09:30 +1100

    updating dashboard

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./components/header";
 import SubHeader from "./components/subheader";
+import OwingTiles from "./components/owing_tiles";
+import DashboardTable from "./components/dashboard_table";
 
 function App() {
   return (
@@ -8,6 +10,14 @@ function App() {
       <div id="main">
         <Header text="Welcome back, Alex 👋" />
         <SubHeader text = "Here's your household expenses summary" />
+        Household Balances
+        <div className="container">
+          <OwingTiles></OwingTiles>
+          <OwingTiles></OwingTiles>
+          <OwingTiles></OwingTiles>
+        </div>
+        Transaction History
+        <DashboardTable/>
       </div>
     </>
   );
