b3b8ef7e60c47b3a2012431b862a565f9836556f
akirafeng2 <alexander.feng2@gmail.com>
2023-11-17 23:10:18 +1100

    adding transaction table

--- src/frontend/src/pages/Dashboard/components/dashboard_table.tsx
+++ src/frontend/src/pages/Dashboard/components/dashboard_table.tsx
@@ -1,6 +1,21 @@
 const DashboardTable = () => {
   return (
-    <div className="dashboard_table_box"></div>
+    <div className="dashboard_table_box">
+      <table>
+        <tr>
+          <th className="item">Item Name</th>
+          <th className="date">Transaction Date</th>
+          <th className="payer">Who Paid?</th>
+          <th className="cost">Cost</th>
+        </tr>
+        <tr>
+          <td className="item">Tomato</td>
+          <td className="date">41 Nov 3000</td>
+          <td className="payer">Bob</td>
+          <td className="cost">$49.34</td>
+        </tr>
+      </table>
+    </div>
   )
 }
 
