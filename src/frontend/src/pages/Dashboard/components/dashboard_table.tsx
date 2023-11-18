0ef23315f7e02e5161f96e6cdf3ab09c744d3019
akirafeng2 <alexander.feng2@gmail.com>
2023-11-19 02:35:04 +1100

    adding placeholder table payload

--- src/frontend/src/pages/Dashboard/components/dashboard_table.tsx
+++ src/frontend/src/pages/Dashboard/components/dashboard_table.tsx
@@ -1,4 +1,28 @@
+interface Transaction {
+  t_id: string;
+  item: string;
+  date: string;
+  payer: string;
+  cost: string;
+}
+
 const DashboardTable = () => {
+  const mock_table: Transaction[] = [
+    {
+      t_id: "1",
+      item: "WW Sliced Frozen Strawberries 500g",
+      date: "06 Jul 2023",
+      payer: "Adam",
+      cost: "6.20",
+    },
+    {
+      t_id: "2",
+      item: "Tomato",
+      date: "12 Jul 2023",
+      payer: "A Long Name",
+      cost: "1500.00",
+    },
+  ];
   return (
     <div className="dashboard_table_box">
       <table>
@@ -8,15 +32,17 @@ const DashboardTable = () => {
           <th className="payer">Who Paid?</th>
           <th className="cost">Cost</th>
         </tr>
-        <tr>
-          <td className="item">Tomato</td>
-          <td className="date">41 Nov 3000</td>
-          <td className="payer">Bob</td>
-          <td className="cost">$49.34</td>
-        </tr>
+        {mock_table.map((item) => (
+          <tr key={item.t_id}>
+            <td className="item">{item.item}</td>
+            <td className="date">{item.date}</td>
+            <td className="payer">{item.payer}</td>
+            <td className="cost">{item.cost}</td>
+          </tr>
+        ))}
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardTable
+export default DashboardTable;
