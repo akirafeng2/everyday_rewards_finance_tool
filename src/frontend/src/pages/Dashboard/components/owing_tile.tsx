49ca363b1dc0e28592c04dd9cbccd57b1ea75b6d
akirafeng2 <alexander.feng2@gmail.com>
2023-11-20 20:48:34 +1100

    owings done

--- src/frontend/src/pages/Dashboard/components/owing_tile.tsx
+++ src/frontend/src/pages/Dashboard/components/owing_tile.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-
 interface OwingTileProp {
-    name: string,
-    owes: number
+  name: string;
+  owes: number;
 }
 
-const OwingTile = ({name, owes}: OwingTileProp) => {
-    let owing_statement: string;
+const OwingTile = ({ name, owes }: OwingTileProp) => {
+  let owing_statement: string;
 
-    if (owes < 0) {
-        owing_statement = name+" is owed";
-        owes = owes*-1
-    } else if (owes > 0) {
-        owing_statement = name+" owes";
-    } else {
-        owing_statement = name+" is square";
-    }
+  if (owes < 0) {
+    owing_statement = name + " is owed";
+    owes = owes * -1;
+  } else if (owes > 0) {
+    owing_statement = name + " owes";
+  } else {
+    owing_statement = name + " is square";
+  }
   return (
     <div className="owings_box">
-        <div className="owing_text">{owing_statement}</div>
-        <div>${owes}</div>
+      <div className="hcontainer owings">
+        <div className="profile_circle"></div>
+        <div className="vcontainer">
+          <div className="owing_text">{owing_statement}</div>
+          <div>${owes}</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OwingTile
+export default OwingTile;
