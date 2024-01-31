9f434a85cd5ec346c2baae9b37fe41608018271c
akirafeng2 <alexander.feng2@gmail.com>
2024-01-31 23:37:56 +1100

    user fill in boxes, no backspace

--- src/frontend/src/pages/Household/HouseholdJoin.tsx
+++ src/frontend/src/pages/Household/HouseholdJoin.tsx
@@ -1,31 +1,55 @@
 import "./houeshold.css";
+import { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import Emoji from "./components/emoji";
 
 const HouseholdJoin = () => {
+  const [tileNumber, setTileNumber] = useState(0);
+  
+  const findFocus = () => {
+    document.getElementById(tileNumber.toString())!.focus()
+  };
 
+  const handleTileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(tileNumber)
+    if (tileNumber < 7) {
+        setTileNumber(previousState => previousState + 1)
+    }
+  }
+
+  useEffect(() => {
+    findFocus()
+  }, [tileNumber])
+  
   const inputTiles = () => {
-    let inputTiles = []
-    for (let i=0; i<8; i++) {
-        inputTiles.push(<input className="inputTiles" type="text" key={i} maxLength={1}></input>)
+    let inputTiles = [];
+    for (let i = 0; i < 8; i++) {
+      inputTiles.push(
+        <input className="inputTiles" key={i} type="text" id={i.toString()} maxLength={1} onFocus={findFocus} onInput={handleTileChange}></input>
+      );
     }
     return inputTiles;
-  }
+  };
 
   return (
     <main className="household">
       <div className="title">Join a Household</div>
-        <div className="whiteBox">
-
-          <form>
+      <div className="whiteBox">
+        <form>
           <div className="subTitle">Add House Code</div>
-            <div className="subText">Find the share code in household admin settings</div>
-            {inputTiles()}
-            <br/>
-            <div className="centrediv"><input type="submit"value="Confirm Code" className="button"></input></div>
-            
-          </form>
-        </div>
+          <div className="subText">
+            Find the share code in household admin settings
+          </div>
+          {inputTiles()}
+          <br />
+          <div className="centrediv">
+            <input
+              type="submit"
+              value="Confirm Code"
+              className="button"
+            ></input>
+          </div>
+        </form>
+      </div>
     </main>
   );
 };
