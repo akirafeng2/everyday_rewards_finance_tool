e254314c28212efacff032f93ac3bd8178c89743
akirafeng2 <alexander.feng2@gmail.com>
2024-02-20 20:13:16 +1100

    fix console errors

--- src/frontend/src/components/sidebar/buttons/transactions-sidebar.tsx
+++ src/frontend/src/components/sidebar/buttons/transactions-sidebar.tsx
@@ -8,12 +8,12 @@ const  Transaction = ({fill}: sidebarProps) => {
   return (
         <svg width="26" height="19" viewBox="0 0 26 19" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="Group 74">
-        <path id="Vector" d="M8.25 2H24.5" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-        <path id="Vector_2" d="M8.25 9.5H24.5" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-        <path id="Vector_3" d="M8.25 17H24.5" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-        <path id="Vector_4" d="M2 2.00024H2.01042" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-        <path id="Vector_5" d="M2 9.5H2.01042" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-        <path id="Vector_6" d="M2 17H2.01042" stroke={fill} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        <path id="Vector" d="M8.25 2H24.5" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path id="Vector_2" d="M8.25 9.5H24.5" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path id="Vector_3" d="M8.25 17H24.5" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path id="Vector_4" d="M2 2.00024H2.01042" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path id="Vector_5" d="M2 9.5H2.01042" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path id="Vector_6" d="M2 17H2.01042" stroke={fill} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
         </g>
         </svg>
   );
