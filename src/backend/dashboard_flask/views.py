06172a5454f347473fc5a93c1223c47ede3b94cd
akirafeng2 <alexander.feng2@gmail.com>
2024-02-29 22:59:46 +1100

    error handling for database connection + routes

--- src/backend/dashboard_flask/views.py
+++ src/backend/dashboard_flask/views.py
@@ -1,34 +1,17 @@
-from flask import render_template, Blueprint, jsonify
-from ..user.views import needs_login
-from .totals import get_totals
+from flask import Blueprint, jsonify
 from ..common import verify_session_mod, get_user_id
-from .get_earliest_date import get_earliest_date
+from .get_unsettled_transactions import get_unsettled_transactions
 
-blueprint = Blueprint('home', __name__, template_folder="./templates")
+blueprint = Blueprint('dashboard', __name__, template_folder="./templates")
 
 
 @blueprint.route('/get_earliest_date', methods=["GET",])
 @verify_session_mod
-def get_earliest_date_route():
+def get_unsettled_transactions_route():
     user_id = get_user_id()
-    date = get_earliest_date(user_id)  # date should be the string it needs object
-    response = {"date": date}
-    if date == "formaterror":
-        return jsonify({'error': 'error retrieving date, possible date format issue'}), 400
+    try:
+        transactions = get_unsettled_transactions(user_id)  # date should be the string it needs object
+    except ConnectionError as e:
+        return jsonify({'error': str(e)}), 503
     else:
-        return jsonify(response)
-
-# Old ------------------------------------------------------------------------------------
-
-
-@blueprint.route('/', methods=['GET',])
-@needs_login
-def totals_route():
-
-    spent_accumalated, owes = get_totals()
-
-    return render_template(
-        'totals.html',
-        spent=spent_accumalated,
-        owings=owes,
-    )
+        return jsonify(transactions), 200
