699e45ee780b9588f7bce8a9006232d625f0378b
akirafeng2 <alexander.feng2@gmail.com>
2024-03-01 15:37:38 +1100

    unitttest for get_unsettled_transactions

--- tests/unit_tests/test_dashboard/test_views.py
+++ tests/unit_tests/test_dashboard/test_views.py
@@ -4,12 +4,12 @@ from ..common_fixtures import client
 
 
 @patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_user_id')
-@patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_earliest_date')
-def test_get_earliest_date_route_valid_response(mock_get_date, mock_get_user_id, client):
+@patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_unsettled_transactions')
+def test_get_earliest_date_route_valid_response(mock_get_transaction, mock_get_user_id, client):
     # Given
     # # Set up mock for the function
     mock_get_user_id.return_value = '1'
-    mock_get_date.return_value = "27 January 2001"
+    mock_get_transaction.return_value = "27 January 2001"
 
     # # Set up expected results
     expected_result = {'date': "27 January 2001"}
@@ -23,12 +23,12 @@ def test_get_earliest_date_route_valid_response(mock_get_date, mock_get_user_id,
 
 
 @patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_user_id')
-@patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_earliest_date')
-def test_get_earliest_date_route_invalid_response(mock_get_date, mock_get_user_id, client):
+@patch('everyday_rewards_finance_tool.src.backend.dashboard_flask.views.get_unsettled_transactions')
+def test_get_earliest_date_route_invalid_response(mock_get_transaction, mock_get_user_id, client):
     # Given
     # # set up mock for the function
     mock_get_user_id.return_value = '1'
-    mock_get_date.return_value = "formaterror"
+    mock_get_transaction.return_value = "formaterror"
     # # Set up expected results
     expected_result = {'error': 'error retrieving date, possible date format issue'}
 
